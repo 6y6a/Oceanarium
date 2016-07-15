7fbe9f7485cd1513c5c0d49f2df6b301483bb5fa
6y6a <gubinalexeymain@gmail.com>
2016-07-15 22:02:03 +0300

    Add header

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,12 @@
 $(document).ready(function(){
-    $('body')
+
+    // Slider in header
+    $('.top-slider').slick({
+        autoplay: true,
+        autoplaySpeed: 3000,
+        pauseOnHover: false,
+        draggable: false,
+        nextArrow: $('.arrow-right'),
+        prevArrow: $('.arrow-left')
+    });
 });
